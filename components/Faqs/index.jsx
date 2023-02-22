5be9a0566d9eee3d803e366c776dff5449b7bb03
Rayd <raydi@bk.ru>
2023-02-22 21:21:40 +0300

    fix

--- components/Faqs/index.jsx
+++ components/Faqs/index.jsx
@@ -5,41 +5,49 @@ import './Faqs.sass';
 const Faqs = () => {
 	const [faqs, setfaqs] = useState([
 		{
+			id: 0,
 			question: 'Почему цены на ваши услуги ниже, чем у конкурентов?',
 			answer: 'Мы принимаем оплату различными способами, такими как банковские карты, Qiwi-кошелек, Payeer и криптовалюты.',
 			open: false
 		},
 		{
+			id: 1,
 			question: 'Как вы гарантируете качество услуг?',
 			answer: 'Контент',
 			open: false
 		},
 		{
+			id: 2,
 			question: 'Как я могу узнать о статусе моего заказа?',
 			answer: 'Контент',
 			open: false
 		},
 		{
+			id: 3,
 			question: 'Как быстро выполняется заказ?',
 			answer: 'Контент',
 			open: false
 		},
 		{
+			id: 4,
 			question: 'Какие способы оплаты вы принимаете?',
 			answer: 'Контент',
 			open: false
 		},
 		{
+			id: 5,
 			question: 'Меня не заблокируют?',
 			answer: 'Контент',
 			open: false
 		},
 		{
+			id: 6,
 			question: 'Как я могу связаться с вами?',
 			answer: 'Контент',
 			open: false
 		},
 		{
+			id: 7,
 			question: 'Что произойдет, если мои подписчики или лайки уйдут?',
 			answer: 'Контент',
 			open: false
@@ -62,7 +70,7 @@ const Faqs = () => {
 
 
 			{faqs.map((faq, i) => (
-				<Faq faq={faq} index={i} toggleFAQ={toggleFAQ} />
+				<Faq faq={faq} index={i} key={faq.id} toggleFAQ={toggleFAQ} />
 			))}
 
 		</div>
